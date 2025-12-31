6da62a71e892c9a728707ffb93ca651558284ede
deepakgaikwad2044 <deepaknd9011@gmail.com>
2025-12-31 20:47:31 +0530

    file uploded

---
+++ api.js
@@ -0,0 +1,33 @@
+const lyrics = [
+  {
+    id: 1,
+    name: "Tum Hi Ho",
+    artist: "Arijit Singh",
+    audio: "audio/tum_ho.mp3",
+    image: "images/1.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Pal Pal",
+    artist: "Afusic ft. Talwiinder",
+    audio: "audio/pal_pal.mp3",
+    image: "images/2.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Dooron Dooron",
+    artist: "Paresh Pahuja",
+    audio: "audio/dooron_dooron.mp3",
+    image: "images/3.jpg",
+  },
+
+  {
+    id: 4,
+    name: "Soulmate",
+    artist: "Badshah & Arijit Singh",
+    audio: "audio/soulmate.mp3",
+    image: "images/4.jpg",
+  },
+];
